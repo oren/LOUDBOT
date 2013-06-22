ce4cf6650784558d691173e568b3d84cb7d51940
Oren <ogolan@yp.com>
2013-06-22 16:36:04 -0700

    get only 50 tweets

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ var T = new Twit(config);
 // https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2&trim_user=true
 var options = {
   screen_name: 'loudbot', 
-  count: 100,
+  count: 50,
   trim_user: true,
   exclude_replies: true, 
   include_rts: false
 };
 
 module.exports = function(cb) {
-  var index = Math.floor(Math.random() * 99);
+  var index = Math.floor(Math.random() * 49);
   var tweet = '';
 
   T.get('/statuses/user_timeline', options, function(err, reply) {
