c896740353d978fcb7bc7265c2c4bb411d6f3ac7
Oren <ogolan@yp.com>
2013-06-22 15:18:14 -0700

    test

---
+++ README.md
@@ -0,0 +1,54 @@
+# Get a random tweet from @LOUDBOT
+
+## Index
+
+* [Example](#example)
+* [Methods](#methods)
+* [Install](#install)
+* [Test](#test)
+
+## Example
+
+```js
+export LOUD_CONFIG=config.js
+loud = require('loudbot');
+loud();
+
+// Output: GUYS I TRIED TO USE A MULTITHREADED REGEXP TO PARSE HTML BUT IT DIDN'T WORK: HELP???
+```js
+
+`LOUD_CONFIG` is an environment variable with the relative path to the config file. The file should look like:
+
+```js
+'use strict';
+
+module.exports = {
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
+};
+```
+
+You get those keys after creating application on Twitter.  
+If you don't have a twitter application, create one [here](https://dev.twitter.com/apps).
+
+## Methods
+
+```js
+var loud = require('loudbot');
+```
+
+### loud(cb)
+
+`cb(err, tweet)` is an optional callback. It fires with a random tweet as a string.
+
+## Install
+
+    npm install loudbot
+
+## Test
+
+    export LOUD_CONFIG=config.js
+    npm test
+
---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Get random tweet from @LOUDBOT
+// 
+// Usage:
+// export LOUD_CONFIG=config.js   // relative path to config file
+// loud = require('loudbot');
+// loud();
+// output: GUYS I TRIED TO USE A MULTITHREADED REGEXP TO PARSE HTML BUT IT DIDN'T WORK: HELP???
+
+// On error you'll get something similar to this
+// { statusCode: 401,
+//   data: '{"errors":[{"message":"Could not authenticate you","code":32}]}',
+//   twitterReply: '{"errors":[{"message":"Could not authenticate you","code":32}]}' 
+//  }
+
+var Twit = require('twit');
+
+var config = null;
+
+if (process.env.LOUD_CONFIG) {
+  config = require('./' + process.env.LOUD_CONFIG);
+} else {
+  throw new Error('LOUD_CONFIG environment variable does\'t exist');
+};
+
+var T = new Twit(config);
+
+// https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2&trim_user=true
+var options = {
+  screen_name: 'loudbot', 
+  count: 100,
+  trim_user: true,
+  exclude_replies: true, 
+  include_rts: false
+};
+
+module.exports = function(cb) {
+  var index = Math.floor(Math.random() * 99);
+  var reply = '';
+
+  T.get('/statuses/user_timeline', options, function(err, reply) {
+    if (err) {
+      if (cb) { return cb(err); };
+      return console.error(err);
+    };
+
+    reply = reply[index].text.replace(/^@[\S]+ /, '');
+    if (cb) { return cb(null, reply) };
+    return console.log(reply);
+  });
+};
